3328bc68884cf7ea6aa8a09b1d3cec64e07d9246
mantran55 <145571137+mantran55@users.noreply.github.com>
2026-07-03 10:43:55 +0700

    Update server.js

--- server.js
+++ server.js
@@ -573,14 +573,19 @@ function columnLetter(col) {
 
 async function getSheetsClient() {
   const credentials =
-  JSON.parse(process.env.GOOGLE_CREDENTIALS);
+    JSON.parse(process.env.GOOGLE_CREDENTIALS);
 
-const auth = new google.auth.GoogleAuth({
-  credentials,
-  scopes: [
-    "https://www.googleapis.com/auth/spreadsheets"
-  ]
-});
+  const auth = new google.auth.GoogleAuth({
+    credentials,
+    scopes: [
+      "https://www.googleapis.com/auth/spreadsheets"
+    ]
+  });
+
+  return google.sheets({
+    version: "v4",
+    auth
+  });
 }
 
 
